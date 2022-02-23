8b778fb58e8cc24c1f1d14bfe4f14193d8ec350d
Matheus de Melo Ribeiro <matheusxp30@gmail.com>
2022-02-22 21:49:28 -0300

    dfnssjdfnkewls

---
+++ ReturnLabelTest/ListViewController/Model/ListModel.swift
@@ -0,0 +1,15 @@
+//
+//  ListModel.swift
+//  ReturnLabelTest
+//
+//  Created by Matheus Ribeiro on 22/02/22.
+//
+
+import UIKit
+
+struct ListModel {
+    struct ListItem {
+        var title: String
+        var color: UIColor
+    }
+}
--- ReturnLabelTest/ViewController.swift
+++ ReturnLabelTest/ViewController.swift
@@ -9,31 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
     @IBAction func didTapButton(_ sender: Any) {
-        let viewModel = ListViewModel(itemList: getList())
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let viewController = storyboard.instantiateViewController(withIdentifier: "ListViewController") as? ListViewController {
+            let viewModel = ListViewModel(itemList: getList())
             viewController.viewModel = viewModel
+//            viewController.delegate = self
             present(viewController, animated: true, completion: nil)
         }
     }
     
-    private func getList() -> [String] {
-        let list: [String] = [
-            "item1",
-            "item2",
-            "item3",
-            "item4",
-            "item5",
-            "item6",
+    private func getList() -> [ListModel.ListItem] {
+        let list: [ListModel.ListItem] = [
+            .init(title: "item1", color: .red),
+            .init(title: "item2", color: .orange),
+            .init(title: "item3", color: .gray),
+            .init(title: "item4", color: .blue),
+            .init(title: "item5", color: .green),
         ]
         
         return list
     }
 }
 
+extension ViewController: ListViewControllerDelegate {
+    func didSelectRow(viewController: ListViewController, item: ListModel.ListItem) {
+        viewController.dismiss(animated: true, completion: nil)
+        titleLabel.text = item.title
+        titleLabel.textColor = item.color
+    }
+}
